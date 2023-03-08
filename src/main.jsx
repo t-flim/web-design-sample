79221799cc93893a8d4b333949a6da3603a162c5
t-flim <fulim1988@gmail.com>
2023-03-08 14:36:42 +0800

    Initial commit  🚀

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+
+ReactDOM.createRoot(document.getElementById("i-am-root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
