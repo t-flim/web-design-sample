b6e766c30d3a531e19503305e4cc14779db25206
t-flim <fulim1988@gmail.com>
2023-03-09 10:44:51 +0800

    Add Products structure code

---
+++ src/pages/Products.jsx
@@ -0,0 +1,12 @@
+
+
+function Products() {
+
+    return (
+        <section id="products" className="section">
+            <h2 className="section-title">products</h2>
+        </section>
+    )
+}
+
+export default Products
