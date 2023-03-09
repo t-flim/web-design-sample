6fe1e9f88aac9753cc515b490a53e8c3355c4695
t-flim <fulim1988@gmail.com>
2023-03-09 10:46:12 +0800

    Add Home structure code

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+
+function Home() {
+
+    return (
+        <section id="home" className="section">
+            <h2 className="section-title">
+                home
+            </h2>
+        </section>
+    )
+}
+
+export default Home
