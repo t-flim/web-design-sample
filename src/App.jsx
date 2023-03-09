a9e5d429ce79a06be32a00721a0353e2570ed2e4
t-flim <fulim1988@gmail.com>
2023-03-09 10:48:18 +0800

    Import and link pages with router component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// pages
+import SharedLayout from "./pages/SharedLayout"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Products from "./pages/Products"
+import Error from "./pages/Error"
 
 function App() {
 
   return (
-    <>
-      Hello, World
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <SharedLayout /> } >
+          <Route index element={ <Home /> } />
+          <Route path="about" element={ <About /> } />
+          <Route path="products" element={ <Products /> } />
+          <Route path="*" element={ <Error /> } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
