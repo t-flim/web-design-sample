fae71babcebaf206bcc0488433e1eb80f3d3864f
t-flim <fulim1988@gmail.com>
2023-03-09 10:44:11 +0800

    Add Error structure code

---
+++ src/pages/Error.jsx
@@ -0,0 +1,14 @@
+
+
+function Error() {
+
+    return (
+        <section id="error-page" className="section">
+            <h2 className="section-title">
+                error 404: page not found
+            </h2>
+        </section>
+    )
+}
+
+export default Error
