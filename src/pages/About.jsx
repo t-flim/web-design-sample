1acfd47af0612ac3901b62e0c40ad27eda08793e
t-flim <fulim1988@gmail.com>
2023-03-09 10:43:41 +0800

    Add About structure code

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+
+
+function About() {
+
+    return (
+        <section id="about" className="section">
+            <h2 className="section-title">
+                our story
+            </h2>
+        </section>
+    )
+}
+
+export default About
